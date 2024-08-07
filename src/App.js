e0ce62ebe880d42b91831ece994b42d12fa793d5
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-08-07 11:22:16 +0300

    Fetched data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchInput from './Components/SearchInput';
-import TransactionForm from './Components/TransactionForm';
-import Transactions from './Components/Transactions';
+import Form from './Components/Form';
+import Transaction from './Components/Transaction';
 
 function App() {
 const title = "FLATIRON BANK";
+const [transactions, setTransactions] = useState([])
+
+useEffect(() => {
+  fetch('http://localhost:3000/transactions')
+  .then(res => res.json())
+  .then(transactions => console.log(transactions))
+  .catch(error => console.log(error))
+},[])
 
   return (
 
    <div className='App'>
       <h1 className='title'>{title}</h1>
-      <TransactionForm /> 
+      <Form /> 
       <SearchInput />
-      <Transactions />
+      <Transaction />
     </div>
   )
 }
